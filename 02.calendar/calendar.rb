37ab5144b0002318c87a9731d9478fc29529a50d
saito40test <kairi.saitou@amd-lab.com>
2022-09-08 10:36:22 +0900

    必要ないメソッドの削除

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -19,20 +19,13 @@ year = Date.today.year
 month = params[:m].to_i if params[:m]
 year = params[:y].to_i if params[:y]
 
-# cur_date = Date.new(year, month, day)
-# p cur_date
-
 # カレンダー作成
 cur_end_day = Date.new(year, month, -1).day
 
 calender = ""
 calender += " #{month}月 #{year}        \n\r"
 calender += "日 月 火 水 木 金 土  \n\r"
-#                 1  2  
-#  3  4  5  6  7  8  9  
-# 10 11 12 13 14 15 16  
-# 17 18 19 20 21 22 23  
-# 24 25 26 27 28 29 30
+
 first_week_day = Date.new(year, month, 1).wday
 (1..first_week_day).each do |_|
     calender += "   "
@@ -41,15 +34,7 @@ end
 (1..cur_end_day).each do |day|
     calender += " "
     calender += " " if day < 10
-
-    if day == Date.today.day
-        print "\e[31m"
-        calender += "#{day}"
-        print "\e[0m"
-    else
-        calender += "#{day}"
-    end
-    
+    calender += "#{day}"
     calender += "\n\r" if Date.new(year, month, day).wday == 6
 end
 
